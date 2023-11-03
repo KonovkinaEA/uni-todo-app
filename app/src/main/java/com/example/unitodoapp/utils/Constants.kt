b893de47a328717f3d18d95fe9f7cb313573c964
shama <shamarorostislav@gmail.com>
2023-11-03 19:50:17 +0300

    Connect, read, write timeouts fix

--- app/src/main/java/com/example/unitodoapp/utils/Constants.kt
+++ app/src/main/java/com/example/unitodoapp/utils/Constants.kt
@@ -4,10 +4,10 @@ const val IMPORTANCE_IMPORTANT_ID = 3
 const val IMPORTANCE_BASIC_ID = 2
 const val IMPORTANCE_LOW_ID = 1
 
-const val CONNECT_TIMEOUT = 120L
-const val READ_TIMEOUT = 120L
-const val WRITE_TIMEOUT = 90L
+const val CONNECT_TIMEOUT = 1L
+const val READ_TIMEOUT = 1L
+const val WRITE_TIMEOUT = 1L
 
-const val RETRY_COUNT = 3
+const val RETRY_COUNT = 1
 
 const val REPEAT_LOAD_INTERVAL = 8L
