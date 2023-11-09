12b1402ffcb3ae4e1df4f7467582359588ed3fdf
sema <buglaev.sa@edu.spbstu.ru>
2023-11-09 10:38:13 +0300

    updated serializable class

---
+++ app/src/main/java/com/example/unitodoapp/data/datastore/UserPreferences.kt
@@ -0,0 +1,11 @@
+package com.example.unitodoapp.data.datastore
+
+import com.example.unitodoapp.ui.screens.settings.model.ThemeMode
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserPreferences(
+    val themeMode: ThemeMode = ThemeMode.LIGHT,
+    val notifyPermissionGranted: Boolean = false,
+    val isListFilter: Boolean = false,
+)
